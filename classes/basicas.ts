4b268715984e2777ae85e80c57f64b9a11a5dd99
mdelbazi <martindelbazi2003@gmail.com>
2023-04-18 18:26:28 -0300

    JAJA

--- classes/basicas.ts
+++ classes/basicas.ts
@@ -1,15 +1,24 @@
 (()=>{
 
-    class Avenger {
-        private name:string;
-        private team:string;
-        public realName: string;
+    class Avenger    {
+        // private name:string;
+        // private team:string;
+        // public realName?: string;
         static avgAge: number = 35;
 
-        
+        constructor (
+            private name:string,
+            private team:string,
+            public realName?:string,
+            ) {}
+
+
+        const antman: Avenger = new Avenger('Antman', 'Avenger', 'Jeff');
+        console.log(antman);
 
 
     }
+
 
 
 })()
--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 "use strict";
 (() => {
     class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+            this.antman = new Avenger('Antman', 'Avenger', 'Jeff');
+        }
     }
+    // private name:string;
+    // private team:string;
+    // public realName?: string;
     Avenger.avgAge = 35;
 })();
 //# sourceMappingURL=main.js.map
